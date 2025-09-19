6017deccfaf33b8e9500bc3a0965543efe84a42e
OmarArtifically123 <abubaker.omar2006@gmail.com>
2025-09-20 01:32:52 +0400

    fix: actually solved rest of routing problems (hopefully)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api, { pick } from "./api";
+import api, { pick } from "../api";
 import { toast } from "./Toast";
 
 export default function Dashboard({ user, openAuth }) {
