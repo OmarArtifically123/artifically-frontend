0d0b336d276355bf8dd927428f3600a26d425b0f
OmarArtifically123 <abubaker.omar2006@gmail.com>
2025-09-28 01:50:03 +0400

    Fix: Fixed 'Million' Package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { million } from 'million/compiler'
+import million from 'million/compiler'
 import { resolve } from 'path'
 
 export default defineConfig({
