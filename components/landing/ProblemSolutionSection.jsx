2a3e6ea7c4102c921bbe0017a2de6179921c8821
OmarArtifically123 <abubaker.omar2006@gmail.com>
2025-10-20 00:01:47 +0400

    More Fixes.

--- components/landing/ProblemSolutionSection.jsx
+++ components/landing/ProblemSolutionSection.jsx
@@ -73,7 +73,6 @@ export default function ProblemSolutionSection() {
 
     const handlePointerUp = () => {
       setIsDragging(false);
-      setPosition(0.5);
     };
 
     window.addEventListener("pointermove", handlePointerMove);
